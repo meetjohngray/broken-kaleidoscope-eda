90450cfc6a423d139c49bc6fe091b49e3bdff587
jgray1 <john.edwin.gray@gmail.com>
2020-11-03 11:42:15 +1300

    add random hex color generator to state.style

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 class Pixel extends React.Component {
+  
   constructor(props) {
     super(props)
     this.state = {
       style: {
-        backgroundColor: 'green',
+        backgroundColor: randomHexColor(),
         height: '50px',
         width: '50px'
       }  
